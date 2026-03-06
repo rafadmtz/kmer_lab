b8768b5eb023501a9274fac326387006a54dd854
Rafael Diaz Martinez <rafadiaz@lcg.unam.mx>
2026-03-06 14:39:39 -0600

    casos prueba

---
+++ kmer_lab.py
@@ -0,0 +1,27 @@
+# Generar k-mers a partir de una secuencia de DNA
+
+# 1. Leer la secuencia de DNA
+seq = input("DNA:").upper()
+
+if not seq:
+    print("Secuencia vacia.")
+    exit(0)
+
+#Validar secuencia
+for x in seq:
+    if x not in "ACTG":
+        print(f"Caracter no valido: {i}")
+        exit(0)
+
+
+# 2. Leer el valor de k
+k =  int(input("k (int): "))
+
+# 3. Recorrer la secuencia con una ventana de tamañASAo k
+for i in range(len(seq) - k + 1):
+     
+    # 4. Extraer el k-mer de la posición actual
+    kmer = seq[i:i-k]
+
+    # 5. Mostrar el k-mer
+    print(f"Posicion {i} --> {kmer}")
